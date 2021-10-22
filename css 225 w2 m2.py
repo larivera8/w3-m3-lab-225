34ffe07f0bd06f7908c07a164f226cc6a31971cb
larivera8 <92804930+larivera8@users.noreply.github.com>
2021-10-22 15:52:23 -0500

    Add files via upload

---
+++ css 225 w2 m2.py
@@ -0,0 +1,44 @@
+#Luis Rivera
+#10-19-2021
+
+#Problem 1
+#This Program will display Hello World to the screen.
+print ("Hello world")
+
+#Problem 2
+#This program will display a message asking the user for its name to great them.
+
+name = input ("Please enter your name: ")
+print ("Hello", name)
+
+#Program 3
+#this program will display a interactive mini conversation.
+
+name2 = input ("Name of the proffesor: ")
+print ("Hello", name2)
+
+#program 4
+#with this program we will try to calculate the area of a circle using given number of radius.
+
+pi = 3.14
+r = float (input("What is your radius: "))
+
+mean = str( p1 + r**2)
+
+#problem 5
+#with this program we will compute the MPG of a trip using a calculation formula.
+
+miles = float (input ("Please enter the number of miles driven"))
+gallons = float (input ("Please enter the number of gallons used"))
+
+mpg = miles / gallons
+
+print ("Your MPG in a car is", mpg)
+
+#Problem 6
+#in this prgram we will convert the given fahrenheit to degrees celsius
+
+fahrenheit = float (input("Enter temperature in fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print ("your given fahrenheit in celsius is", celsius)
+       
